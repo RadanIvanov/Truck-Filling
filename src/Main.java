fe99b88fa0e32ef4d337848de16d9c429baa4b28
RadanIvanov <radan.ivanov04@gmail.com>
2024-11-12 15:03:47 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -12,7 +12,7 @@ public class Main {
         pallets.add(new Container(12, 15, 12));
         palletsVolume(pallets);
 
-        /* doesn't remove well
+        /*
         ArrayList<Container> palletsCopy = new ArrayList<>(pallets);
         pallets.addAll(rotateAroundX(palletsCopy));
         pallets.addAll(rotateAroundY(palletsCopy));
